ee7434b7116e4458d5e39b90b2e965b96db5d606
Rosanne Hoyem <rosiehoyem@gmail.com>
2012-06-11 10:00:02 -0500

    First git commit, added all things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mnproject::Application.config.secret_token = '771020e12076ce57a9e4eeda622f6d51b417c5ae40af7f89c1dce4eca86709474efd97e7b0ff1ee5b265dba3a39557ab60df75299a93c92a8b4c990ca6b28b72'
